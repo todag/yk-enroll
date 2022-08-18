3f269a34598434775442997bab4ebc6d1e91f95f
todag <33574714+todag@users.noreply.github.com>
2022-08-18 21:53:34 +0200

    Change datagrid display of enabled applications.

---
+++ YKEnroll.Win/Views/Converters/YubiKeyApplicationsArrayToCountConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace YKEnroll.Win.Views.Converters
+{
+    public sealed class YubiKeyApplicationsArrayToCountConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is string[])
+            {
+                string[] apps = (string[])value;
+                if (apps.Count() == 0)
+                    return 0;
+                else if (apps.Count() == 1 && apps[0] == "None")
+                    return 0;
+                else
+                    return apps.Count();
+            }
+            else
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
